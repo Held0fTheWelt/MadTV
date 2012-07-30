5821e0e9f15ea0bfb8feb416e656f9940e5c9949
Yves Tanas <Yves.Tanas@gmx.de>
2012-07-31 00:51:20 +0200

    Major Change
    got rid of Loop.js !

--- client.js
+++ client.js
@@ -1,9 +1,69 @@
 var Client = IgeClass.extend({
 	classId: 'Client',
 
+    loadBackground:function () {
+        // Create our background
+        return new GameElement()
+            .id('background')
+            .depth(0)
+            .width(1200)
+            .height(2250)
+            .texture(gameTexture[0])
+            .translateTo(0, -140, 0)
+            .addComponent(IgeVelocityComponent);
+    },
+    loadSkyscraper: function () {
+        // Create the Skyscraper
+        return new Skyscraper().depth(1)
+            .translateTo(0, 310, 0)
+            .addComponent(IgeVelocityComponent);
+    },
+    loadSkyscraperRooms: function() {
+        // Create the Skyscraper
+        return new Rooms().depth(1);
+    },
+    loadElevator: function() {
+        return new FloorScene().depth(2).translateTo(2, -47, 0);
+    },
+    loadCharacter: function(){
+        return new Character().depth(2).translateTo(0, 5, 0)
+            .addComponent(PlayerComponent)
+            .setType(4);
+
+    },
+    getFloorsHeight: function(floornumber){
+        switch(floornumber){
+            case 0: return 0;
+                break;
+            case 1: return -100;
+                break;
+            case 2: return -200;
+                break;
+            case 3: return -300;
+                break;
+            case 4: return -400;
+                break;
+            case 5: return -500;
+                break;
+            case 6: return -600;
+                break;
+            case 7: return -700;
+                break;
+            case 8: return -800;
+                break;
+            case 9: return -900;
+                break;
+            case 10: return -1000;
+                break;
+            case 11: return -1100;
+                break;
+            case 12: return -1200;
+                break;
+        }
+    },
 	init: function () {
 		// Load our textures
-		var self = this,
+        var rootNode = this;
 		gameTexture = [];
 
 		this.obj = [];
@@ -18,25 +78,26 @@ var Client = IgeClass.extend({
 				// Check if the engine started successfully
 				if (success) {
 					// Create the scene
-					self.scene1 = new IgeScene2d();
+                    rootNode.scene1 = new IgeScene2d();
 					
 
 					// Create the main viewport
-					self.vp1 = new IgeViewport()
+                    rootNode.vp1 = new IgeViewport()
 						.autoSize(true)
-						.scene(self.scene1)
+						.scene(rootNode.scene1)
 						.drawBounds(false)
 						.mount(ige);
 
-					// Create an entity
-					self.obj[0] = new Loop()
-						.mount(self.scene1);
-
+                    rootNode.obj[0] = rootNode.loadBackground().mount(rootNode.scene1);
+                    rootNode.obj[1] = rootNode.loadSkyscraper().mount(rootNode.scene1);
+                    rootNode.obj[2] = rootNode.loadSkyscraperRooms().mount(rootNode.obj[1]);
+                    rootNode.obj[3] = rootNode.loadElevator().mount(rootNode.obj[1]);
+                    rootNode.obj[4] = rootNode.loadCharacter().mount(rootNode.obj[3]);
                     // Create the UI scene
-                    self.obj[1] = new IgeScene2d().depth(2)
-                        .mount(self.scene1);
+                    rootNode.obj[5] = new IgeScene2d().depth(2)
+                        .mount(rootNode.scene1);
 
-                    self.obj[2] = new IgeUiEntity()
+                    rootNode.obj[6] = new IgeUiEntity()
                         .id('bottomBar')
                         .depth(1)
                         .backgroundColor('#474747')
@@ -49,9 +110,64 @@ var Client = IgeClass.extend({
                         .backgroundPosition(0, 0)
                         .mouseOver(function () {this.backgroundColor('#49ceff'); })
                         .mouseOut(function () {this.backgroundColor('#474747'); })
-                        .mount(self.obj[1]);
+                        .mount(rootNode.obj[5]);
 				}
 			});
+
+            ige.addBehaviour("tick", function () {
+                var y = rootNode.obj[3].currentFloor;
+                switch (y) {
+                    case 0: rootNode.obj[3].currentHeading = 1;
+                        break;
+                    case 1: rootNode.obj[3].currentHeading = 2;
+                        break;
+                    case 2: rootNode.obj[3].currentHeading = 3;
+                        break;
+                    case 3: rootNode.obj[3].currentHeading = 4;
+                        break;
+                    case 4: rootNode.obj[3].currentHeading = 5;
+                        break;
+                    case 5: rootNode.obj[3].currentHeading = 6;
+                        break;
+                    case 6: rootNode.obj[3].currentHeading = 7;
+                        break;
+                    case 7: rootNode.obj[3].currentHeading = 8;
+                        break;
+                    case 8: rootNode.obj[3].currentHeading = 9;
+                        break;
+                    case 9: rootNode.obj[3].currentHeading = 10;
+                        break;
+                    case 10: rootNode.obj[3].currentHeading = 11;
+                        break;
+                    case 11: rootNode.obj[3].currentHeading = 12;
+                        break;
+                    case 12: rootNode.obj[3].currentHeading = 0;
+                        break;
+                }
+                var direction = rootNode.obj[3].currentHeading - rootNode.obj[3].currentFloor;
+
+                if(direction > 0){
+                    rootNode.obj[0].velocity.y(0.015);
+                    rootNode.obj[1].velocity.y(0.15);
+                    rootNode.obj[3].obj[0].velocity.y(-0.25);
+                } else if (direction < 0){
+                    rootNode.obj[0].velocity.y(-0.015);
+                    rootNode.obj[1].velocity.y(-0.15);
+                    rootNode.obj[3].obj[0].velocity.y(0.25);
+                } else {
+                    rootNode.obj[0].velocity.y(0);
+                    rootNode.obj[1].velocity.y(0);
+                    rootNode.obj[3].obj[0].velocity.y(0);
+                }
+
+                var elevatorpos = rootNode.obj[3].obj[0].translate().y();
+                elevatorpos-=rootNode.getFloorsHeight(rootNode.obj[3].currentHeading);
+
+                if(elevatorpos  >= -10 && elevatorpos <= 10){
+                    rootNode.obj[3].currentFloor = rootNode.obj[3].currentHeading;
+                }
+
+            });
 		});
 	}
 });
